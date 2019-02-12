b91116fa4dd65bad50ea11997b80d75d61a2d6c8
Robert Thompson <robertpt@hotmail.co.uk>
2019-02-12 11:51:11 +0000

    Added Console Application and a few test images
    C# Console app will output to the Ardunio

---
+++ ImagePharser/ImagePharser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO.Ports;
+
+namespace ImagePharser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+            Bitmap img = new Bitmap("#");
+            for(int x=0;x<img.Width;x++)
+            {
+                for(int y=0;y<img.Height;y++)
+                {
+                    Color pixel = img.GetPixel(x, y);
+                    if(pixel.ToArgb().Equals(Color.Black.ToArgb()))
+                    {
+                        output = output + "1";
+                    }
+                    else
+                    {
+                        output = output + "0";
+                    }
+                }
+                output = output + ",";
+            }
+            SerialPort serial = new SerialPort("COM8", 9600);
+            serial.Open();
+            serial.Write(output);
+            serial.Close();
+            Debug.WriteLine(output);
+        }
+    }
+}
